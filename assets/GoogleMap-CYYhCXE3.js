4e19f3460232b2bac5c59aeac69b8bb4a64b532d
Hudayfa Koujdal <102149280+hdif004@users.noreply.github.com>
2026-04-12 14:19:55 +0100

    Updates

---
+++ assets/GoogleMap-CYYhCXE3.js
@@ -0,0 +1,10 @@
+import{j as e}from"./index-BlpGUuv6.js";import"./react-vendor-DSQSPd6x.js";import{M as r,T as s,a as i,P as n,L as t}from"./ui-vendor-CWueu2ty.js";import{a as l}from"./Navbar-CF3fWTim.js";delete t.Icon.Default.prototype._getIconUrl;t.Icon.Default.mergeOptions({iconRetinaUrl:"https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon-2x.png",iconUrl:"https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png",shadowUrl:"https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png"});const p=t.divIcon({className:"",html:`
+    <div style="
+      width: 32px; height: 32px;
+      background: #048162;
+      border: 3px solid white;
+      border-radius: 50% 50% 50% 0;
+      transform: rotate(-45deg);
+      box-shadow: 0 2px 8px rgba(4,129,98,0.4);
+    "></div>
+  `,iconSize:[32,32],iconAnchor:[16,32],popupAnchor:[0,-36]}),o=33.589170953186944,a=-7.596696157143334,g=()=>e.jsxs(r,{center:[o,a],zoom:17,scrollWheelZoom:!1,style:{width:"100%",height:"100%",minHeight:"288px"},className:"rounded-2xl z-0",children:[e.jsx(s,{attribution:'© <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> © <a href="https://carto.com/">CARTO</a>',url:"https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}{r}.png"}),e.jsx(i,{position:[o,a],icon:p,children:e.jsx(n,{children:e.jsxs("div",{style:{display:"flex",flexDirection:"column",alignItems:"center",gap:"6px",minWidth:"140px"},children:[e.jsx("img",{src:l("AmadalGreen.png"),alt:"Amadal",style:{height:"28px",width:"auto",objectFit:"contain"}}),e.jsxs("div",{style:{fontSize:"11px",color:"#6b7280",textAlign:"center",lineHeight:"1.5"},children:["121, rue Radi Slaoui, Belvédère",e.jsx("br",{}),"Casablanca"]})]})})})]});export{g as L};
